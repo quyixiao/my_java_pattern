372a5fb31906ce7d4adeaf0497dcd0b09dd97846
quyixiao <2621048238@qq.com>
2020-05-11 19:42:48 +0800

    提交修改

--- src/main/java/com/design/pattern/proxy/dynamic/JDKCallBackTest.java
+++ src/main/java/com/design/pattern/proxy/dynamic/JDKCallBackTest.java
@@ -11,7 +11,6 @@ public class JDKCallBackTest {
                     return "xxxxxxxxx";
                 }
             });
-
             iCallBackxx.callBack();
         } catch (Exception e) {
             e.printStackTrace();
